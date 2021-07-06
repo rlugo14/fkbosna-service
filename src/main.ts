de95ea7827e96b4687ef5937b8713410c5e185eb
Raul Lugo <rlugo14@gmail.com>
2021-07-06 02:13:47 +0200

    accept PORT env but use default

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ? process.env.PORT : 3010
+
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  Logger.log("Running on port 3000", "NestApplication")
-  await app.listen(3000);
+  Logger.log(`Running on port ${port}`, "NestApplication")
+  await app.listen(port);
 }
 bootstrap();
