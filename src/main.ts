3417a964505a5cab59196a678855a6d7c9e7b7d3
Raul Lugo <rlugo14@gmail.com>
2023-02-08 22:13:30 +0100

    enable cors with NestFactory

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
   const port = process.env.PORT ? process.env.PORT : 3010;
 
   Logger.log(`Running on port ${port}`, 'NestApplication');
