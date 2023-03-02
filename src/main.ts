390d8b00eeac0cea29b7021c5cec196f70454916
Raul Lugo <rlugo14@gmail.com>
2023-03-02 21:57:05 +0100

    readd long cors configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders:
+      'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
+  });
 
   const port = process.env.PORT ? process.env.PORT : 3010;
 
