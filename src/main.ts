09ba24077b8ff8803b2ba2e81deedf385ed56065
Raul Lugo <rlugo14@gmail.com>
2023-03-16 21:07:52 +0100

    add public serve for public folder

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -13,6 +15,7 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT ? process.env.PORT : 3010;
+  app.use('/public', express.static(join(__dirname, '..', 'public')));
 
   Logger.log(`Running on port ${port}`, 'NestApplication');
   await app.listen(port);
