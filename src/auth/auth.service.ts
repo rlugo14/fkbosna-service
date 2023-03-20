fa7b1fd8db0d2e337afeb9826496589ee1baf697
Raul Lugo <rlugo14@gmail.com>
2023-03-20 16:27:02 +0100

    use nest jwt package instead of jsonwebtoken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as jwt from 'jsonwebtoken';
+import { JwtService } from '@nestjs/jwt';
 import { TokenPayload } from './interfaces/token.payload';
 
 const MINS_15_IN_SECONDS = 15 * 60;
@@ -8,12 +9,19 @@ const MINS_15_IN_SECONDS = 15 * 60;
 @Injectable()
 export class AuthService {
   private secret: string;
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {
     this.secret = this.configService.get<string>('JWT_SECRET');
   }
   createToken({ userId, email, tenantId }: TokenPayload) {
-    return jwt.sign({ userId, email, tenantId }, this.secret, {
-      expiresIn: MINS_15_IN_SECONDS,
-    });
+    return this.jwtService.signAsync(
+      { userId, email, tenantId },
+      {
+        secret: this.secret,
+        expiresIn: MINS_15_IN_SECONDS,
+      },
+    );
   }
 }
