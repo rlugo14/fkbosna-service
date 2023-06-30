6037a0ad9594eb01181deb5025c54cfbf96eceec
Raul Lugo <rlugo14@gmail.com>
2023-06-30 15:56:42 +0100

    add edge case in last day of month

--- src/helpers/getMonthLimits.ts
+++ src/helpers/getMonthLimits.ts
@@ -1,6 +1,6 @@
 const getMonthLimits = (month: number, year: number) => ({
   firstDay: new Date(year, month, 1),
-  lastDay: new Date(year, month + 1, 0),
+  lastDay: new Date(year, month + 1, 0, 23, 59, 59),
 });
 
 export default getMonthLimits;
