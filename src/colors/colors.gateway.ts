688385ef5acbaafb3cfb6dae3fd4b24dfb378898
Raul Lugo <rlugo14@gmail.com>
2023-11-02 00:47:10 +0100

    add new websocket event (updateHome)

--- src/colors/colors.gateway.ts
+++ src/colors/colors.gateway.ts
@@ -12,16 +12,25 @@ export class ColorsGateway {
   @WebSocketServer() io: Namespace;
 
   @SubscribeMessage('joinRoom')
-  async handleJoinRoom(socket: Socket, tenantSlug: string) {
-    socket.join(tenantSlug);
-    this.logger.log(`${socket.id} joined room ${tenantSlug}`);
-    const socketsInRoom = await this.io.in(tenantSlug).fetchSockets();
+  async handleJoinRoom(socket: Socket, roomName: string) {
+    socket.join(roomName);
+    this.logger.log(`socket: ${socket.id} joined room ${roomName}`);
+    const socketsInRoom = await this.io.in(roomName).fetchSockets();
     this.logger.log(
-      `Amount of sockets in room "${tenantSlug}": ${socketsInRoom.length}`,
+      `Amount of sockets in room "${roomName}": ${socketsInRoom.length}`,
     );
   }
 
+  @SubscribeMessage('triggerUpdateHome')
+  triggerHomeUpdate(socket: Socket, roomName: string) {
+    this.logger.log(
+      `socket: ${socket.id} triggered updateHome for room: ${roomName}`,
+    );
+    this.notifySocketsInRoom(roomName);
+  }
+
   notifySocketsInRoom(roomName: string) {
+    console.log(roomName);
     this.logger.log(`emitting update signal to room: ${roomName}`);
     this.io.to(roomName).emit('updateHome');
   }
